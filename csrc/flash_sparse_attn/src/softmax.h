69dca0d02347abb26e971eaadb7f96a916cab7b1
LoserCheems <losercheems@gmail.com>
2025-12-22 00:55:39 +0800

    Cleans up softmax helper formatting
    Improves readability by expanding template declarations, arguments, and loops onto separate lines and aligning style with project conventions

--- csrc/flash_sparse_attn/src/softmax.h
+++ csrc/flash_sparse_attn/src/softmax.h
@@ -19,8 +19,19 @@ using namespace cute;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-template<bool zero_init=true, typename Engine0, typename Layout0, typename Engine1, typename Layout1, typename Operator>
-__device__ __forceinline__ void thread_reduce_(Tensor<Engine0, Layout0> const &tensor, Tensor<Engine1, Layout1> &summary, Operator &op) {
+template <
+    bool zero_init = true,
+    typename Engine0,
+    typename Layout0,
+    typename Engine1,
+    typename Layout1,
+    typename Operator
+>
+__device__ __forceinline__ void thread_reduce_(
+    Tensor<Engine0, Layout0> const &tensor,
+    Tensor<Engine1, Layout1> &summary,
+    Operator &op
+) {
     static_assert(Layout0::rank == 2, "Only support 2D Tensor");
     static_assert(Layout1::rank == 1, "Only support 1D Tensor");
     CUTE_STATIC_ASSERT_V(size<0>(summary) == size<0>(tensor));
@@ -34,36 +45,81 @@ __device__ __forceinline__ void thread_reduce_(Tensor<Engine0, Layout0> const &t
     }
 }
 
-template<typename Engine0, typename Layout0, typename Engine1, typename Layout1, typename Operator>
-__device__ __forceinline__ void quad_allreduce_(Tensor<Engine0, Layout0> &dst, Tensor<Engine1, Layout1> &src, Operator &op) {
+template <
+    typename Engine0,
+    typename Layout0,
+    typename Engine1,
+    typename Layout1,
+    typename Operator
+>
+__device__ __forceinline__ void quad_allreduce_(
+    Tensor<Engine0, Layout0> &dst,
+    Tensor<Engine1, Layout1> &src,
+    Operator &op
+) {
     CUTE_STATIC_ASSERT_V(size(dst) == size(src));
     #pragma unroll
-    for (int i = 0; i < size(dst); i++){
+    for (int i = 0; i < size(dst); i++) {
         dst(i) = Allreduce<4>::run(src(i), op);
     }
 }
 
-template<bool zero_init=true, typename Engine0, typename Layout0, typename Engine1, typename Layout1, typename Operator>
-__device__ __forceinline__ void reduce_(Tensor<Engine0, Layout0> const& tensor, Tensor<Engine1, Layout1> &summary, Operator &op) {
+template <
+    bool zero_init = true,
+    typename Engine0,
+    typename Layout0,
+    typename Engine1,
+    typename Layout1,
+    typename Operator
+>
+__device__ __forceinline__ void reduce_(
+    Tensor<Engine0, Layout0> const &tensor,
+    Tensor<Engine1, Layout1> &summary,
+    Operator &op
+) {
     thread_reduce_<zero_init>(tensor, summary, op);
     quad_allreduce_(summary, summary, op);
 }
 
-template<bool zero_init=true, typename Engine0, typename Layout0, typename Engine1, typename Layout1>
-__device__ __forceinline__ void reduce_max(Tensor<Engine0, Layout0> const& tensor, Tensor<Engine1, Layout1> &max){
+template <
+    bool zero_init = true,
+    typename Engine0,
+    typename Layout0,
+    typename Engine1,
+    typename Layout1
+>
+__device__ __forceinline__ void
+reduce_max(Tensor<Engine0, Layout0> const &tensor, Tensor<Engine1, Layout1> &max) {
     MaxOp<float> max_op;
     reduce_<zero_init>(tensor, max, max_op);
 }
 
-template<bool zero_init=true, typename Engine0, typename Layout0, typename Engine1, typename Layout1>
-__device__ __forceinline__ void reduce_sum(Tensor<Engine0, Layout0> const& tensor, Tensor<Engine1, Layout1> &sum){
+template <
+    bool zero_init = true,
+    typename Engine0,
+    typename Layout0,
+    typename Engine1,
+    typename Layout1
+>
+__device__ __forceinline__ void
+reduce_sum(Tensor<Engine0, Layout0> const &tensor, Tensor<Engine1, Layout1> &sum) {
     SumOp<float> sum_op;
     thread_reduce_<zero_init>(tensor, sum, sum_op);
 }
 
 // Apply the exp to all the elements.
-template <bool Scale_max=true, typename Engine0, typename Layout0, typename Engine1, typename Layout1>
-__forceinline__ __device__ void scale_apply_exp2(Tensor<Engine0, Layout0> &tensor, Tensor<Engine1, Layout1> const &max, const float scale) {
+template <
+    bool Scale_max = true,
+    typename Engine0,
+    typename Layout0,
+    typename Engine1,
+    typename Layout1
+>
+__forceinline__ __device__ void scale_apply_exp2(
+    Tensor<Engine0, Layout0> &tensor,
+    Tensor<Engine1, Layout1> const &max,
+    const float scale
+) {
     static_assert(Layout0::rank == 2, "Only support 2D Tensor");
     static_assert(Layout1::rank == 1, "Only support 1D Tensor");
     CUTE_STATIC_ASSERT_V(size<0>(max) == size<0>(tensor));
@@ -74,7 +130,7 @@ __forceinline__ __device__ void scale_apply_exp2(Tensor<Engine0, Layout0> &tenso
         // If we don't have float around M_LOG2E the multiplication is done in fp64.
         const float max_scaled = max(mi) == -INFINITY ? 0.f : max(mi) * (Scale_max ? scale : float(M_LOG2E));
         #pragma unroll
-        for (int ni = 0; ni < size<1>(tensor); ++ni)  {
+        for (int ni = 0; ni < size<1>(tensor); ++ni) {
             // Instead of computing exp(x - max), we compute exp2(x * log_2(e) -
             // max * log_2(e)) This allows the compiler to use the ffma
             // instruction instead of fadd and fmul separately.
@@ -82,17 +138,28 @@ __forceinline__ __device__ void scale_apply_exp2(Tensor<Engine0, Layout0> &tenso
             // See: https://github.com/pytorch/pytorch/issues/121558 for more details
             // This macro is set in PyTorch and not FlashAttention
             #ifdef UNFUSE_FMA
-                tensor(mi, ni) = exp2f(__fmul_rn(tensor(mi, ni), scale) - max_scaled);
+            tensor(mi, ni) = exp2f(__fmul_rn(tensor(mi, ni), scale) - max_scaled);
             #else
-                tensor(mi, ni) = exp2f(tensor(mi, ni) * scale - max_scaled);
-            #endif
+            tensor(mi, ni) = exp2f(tensor(mi, ni) * scale - max_scaled);
+        #endif
         }
     }
 }
 
 // Apply the exp to all the elements.
-template <bool zero_init=true, typename Engine0, typename Layout0, typename Engine1, typename Layout1>
-__forceinline__ __device__ void max_scale_exp2_sum(Tensor<Engine0, Layout0> &tensor, Tensor<Engine1, Layout1> &max, Tensor<Engine1, Layout1> &sum, const float scale) {
+template <
+    bool zero_init = true,
+    typename Engine0,
+    typename Layout0,
+    typename Engine1,
+    typename Layout1
+>
+__forceinline__ __device__ void max_scale_exp2_sum(
+    Tensor<Engine0, Layout0> &tensor,
+    Tensor<Engine1, Layout1> &max,
+    Tensor<Engine1, Layout1> &sum,
+    const float scale
+) {
     static_assert(Layout0::rank == 2, "Only support 2D Tensor");
     static_assert(Layout1::rank == 1, "Only support 1D Tensor");
     CUTE_STATIC_ASSERT_V(size<0>(max) == size<0>(tensor));
@@ -110,7 +177,7 @@ __forceinline__ __device__ void max_scale_exp2_sum(Tensor<Engine0, Layout0> &ten
         const float max_scaled = max(mi) == -INFINITY ? 0.f : max(mi) * scale;
         sum(mi) = 0;
         #pragma unroll
-        for (int ni = 0; ni < size<1>(tensor); ++ni)  {
+        for (int ni = 0; ni < size<1>(tensor); ++ni) {
             // Instead of computing exp(x - max), we compute exp2(x * log_2(e) -
             // max * log_2(e)) This allows the compiler to use the ffma
             // instruction instead of fadd and fmul separately.
@@ -124,18 +191,20 @@ __forceinline__ __device__ void max_scale_exp2_sum(Tensor<Engine0, Layout0> &ten
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-template <int kNRows>
-struct Softmax {
+template <int kNRows> struct Softmax {
 
     using TensorT = decltype(make_tensor<float>(Shape<Int<kNRows>>{}));
     TensorT row_max, row_sum;
 
     __forceinline__ __device__ Softmax() {};
 
-    template<bool Is_first, bool Check_inf=false, typename Tensor0, typename Tensor1>
+    template <bool Is_first, bool Check_inf = false, typename Tensor0, typename Tensor1>
     __forceinline__ __device__ void softmax(Tensor0 &acc_s, Tensor1 &acc_o) {
         // Reshape acc_s from (MMA=4, MMA_M, MMA_N) to (nrow=(2, MMA_M), ncol=(2, MMA_N))
-        Tensor scores = make_tensor(acc_s.data(), FLASH_NAMESPACE::convert_layout_acc_rowcol(acc_s.layout()));
+        Tensor scores = make_tensor(
+            acc_s.data(),
+            FLASH_NAMESPACE::convert_layout_acc_rowcol(acc_s.layout())
+        );
         static_assert(decltype(size<0>(scores))::value == kNRows);
         if (Is_first) {
             FLASH_NAMESPACE::template reduce_max</*zero_init=*/true>(scores, row_max);
@@ -146,7 +215,10 @@ struct Softmax {
             cute::copy(row_max, scores_max_prev);
             FLASH_NAMESPACE::template reduce_max</*zero_init=*/false>(scores, row_max);
             // Reshape acc_o from (MMA=4, MMA_M, MMA_K) to (nrow=(2, MMA_M), ncol=(2, MMA_K))
-            Tensor acc_o_rowcol = make_tensor(acc_o.data(), FLASH_NAMESPACE::convert_layout_acc_rowcol(acc_o.layout()));
+            Tensor acc_o_rowcol = make_tensor(
+                acc_o.data(),
+                FLASH_NAMESPACE::convert_layout_acc_rowcol(acc_o.layout())
+            );
             static_assert(decltype(size<0>(acc_o_rowcol))::value == kNRows);
             #pragma unroll
             for (int mi = 0; mi < size(row_max); ++mi) {
@@ -156,7 +228,9 @@ struct Softmax {
                 float scores_scale = exp2f((scores_max_prev(mi) - scores_max_cur) * float(M_LOG2E));
                 row_sum(mi) *= scores_scale;
                 #pragma unroll
-                for (int ni = 0; ni < size<1>(acc_o_rowcol); ++ni) { acc_o_rowcol(mi, ni) *= scores_scale; }
+                for (int ni = 0; ni < size<1>(acc_o_rowcol); ++ni) {
+                    acc_o_rowcol(mi, ni) *= scores_scale;
+                }
             }
             FLASH_NAMESPACE::scale_apply_exp2(scores, row_max, float(M_LOG2E));
             // We don't do the reduce across threads here since we don't need to use the row_sum.
@@ -165,10 +239,14 @@ struct Softmax {
         }
     };
 
-    template<bool Is_first, bool Check_inf=false, typename Tensor0, typename Tensor1>
-    __forceinline__ __device__ void softmax_rescale_o(Tensor0 &acc_s, Tensor1 &acc_o, float softmax_scale_log2) {
+    template <bool Is_first, bool Check_inf = false, typename Tensor0, typename Tensor1>
+    __forceinline__ __device__ void
+    softmax_rescale_o(Tensor0 &acc_s, Tensor1 &acc_o, float softmax_scale_log2) {
         // Reshape acc_s from (MMA=4, MMA_M, MMA_N) to (nrow=(2, MMA_M), ncol=(2, MMA_N))
-        Tensor scores = make_tensor(acc_s.data(), FLASH_NAMESPACE::convert_layout_acc_rowcol(acc_s.layout()));
+        Tensor scores = make_tensor(
+            acc_s.data(),
+            FLASH_NAMESPACE::convert_layout_acc_rowcol(acc_s.layout())
+        );
         static_assert(decltype(size<0>(scores))::value == kNRows);
         if (Is_first) {
             FLASH_NAMESPACE::template reduce_max</*zero_init=*/true>(scores, row_max);
@@ -179,7 +257,10 @@ struct Softmax {
             cute::copy(row_max, scores_max_prev);
             FLASH_NAMESPACE::template reduce_max</*zero_init=*/false>(scores, row_max);
             // Reshape acc_o from (MMA=4, MMA_M, MMA_K) to (nrow=(2, MMA_M), ncol=(2, MMA_K))
-            Tensor acc_o_rowcol = make_tensor(acc_o.data(), FLASH_NAMESPACE::convert_layout_acc_rowcol(acc_o.layout()));
+            Tensor acc_o_rowcol = make_tensor(
+                acc_o.data(),
+                FLASH_NAMESPACE::convert_layout_acc_rowcol(acc_o.layout())
+            );
             static_assert(decltype(size<0>(acc_o_rowcol))::value == kNRows);
             #pragma unroll
             for (int mi = 0; mi < size(row_max); ++mi) {
@@ -189,7 +270,9 @@ struct Softmax {
                 float scores_scale = exp2f((scores_max_prev(mi) - scores_max_cur) * softmax_scale_log2);
                 row_sum(mi) *= scores_scale;
                 #pragma unroll
-                for (int ni = 0; ni < size<1>(acc_o_rowcol); ++ni) { acc_o_rowcol(mi, ni) *= scores_scale; }
+                for (int ni = 0; ni < size<1>(acc_o_rowcol); ++ni) {
+                    acc_o_rowcol(mi, ni) *= scores_scale;
+                }
             }
             FLASH_NAMESPACE::scale_apply_exp2(scores, row_max, softmax_scale_log2);
             // We don't do the reduce across threads here since we don't need to use the row_sum.
@@ -198,26 +281,31 @@ struct Softmax {
         }
     };
 
-    template<bool Split=false, typename Tensor0>
-    __forceinline__ __device__ TensorT normalize_softmax_lse(Tensor0 &acc_o, float softmax_scale) {
+    template <bool Split = false, typename Tensor0>
+    __forceinline__ __device__ TensorT
+    normalize_softmax_lse(Tensor0 &acc_o, float softmax_scale) {
         SumOp<float> sum_op;
         quad_allreduce_(row_sum, row_sum, sum_op);
         TensorT lse = make_fragment_like(row_sum);
-        Tensor acc_o_rowcol = make_tensor(acc_o.data(), FLASH_NAMESPACE::convert_layout_acc_rowcol(acc_o.layout()));
+        Tensor acc_o_rowcol = make_tensor(
+            acc_o.data(),
+            FLASH_NAMESPACE::convert_layout_acc_rowcol(acc_o.layout())
+        );
         static_assert(decltype(size<0>(acc_o_rowcol))::value == kNRows);
         #pragma unroll
         for (int mi = 0; mi < size<0>(acc_o_rowcol); ++mi) {
             float sum = row_sum(mi);
             float inv_sum = (sum == 0.f || sum != sum) ? 1.f : 1.f / sum;
-            lse(mi) = (sum == 0.f || sum != sum)
-                    ? (Split ? -INFINITY : INFINITY)
-                    : (row_max(mi) + __logf(sum));
+            lse(mi) = (sum == 0.f || sum != sum) ? (Split ? -INFINITY : INFINITY)
+                                                 : (row_max(mi) + __logf(sum));
             float scale = inv_sum;
             #pragma unroll
-            for (int ni = 0; ni < size<1>(acc_o_rowcol); ++ni) { acc_o_rowcol(mi, ni) *= scale; }
+            for (int ni = 0; ni < size<1>(acc_o_rowcol); ++ni) {
+                acc_o_rowcol(mi, ni) *= scale;
+            }
         }
         return lse;
     };
 };
 
-}  // namespace FLASH_NAMESPACE
+} // namespace FLASH_NAMESPACE
