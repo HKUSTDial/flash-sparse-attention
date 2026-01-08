36fd600edce4a02b6fa52daa260e43ce2b60c470
Loser Cheems <losercheems@gmail.com>
2026-01-08 21:53:46 +0800

    Initializes softmax accumulators
    Ensures row maxima start at negative infinity and sums at zero so subsequent softmax passes receive deterministic initial values

--- csrc/flash_sparse_attn/src/softmax.h
+++ csrc/flash_sparse_attn/src/softmax.h
@@ -196,7 +196,13 @@ template <int kNRows> struct Softmax {
     using TensorT = decltype(make_tensor<float>(Shape<Int<kNRows>>{}));
     TensorT row_max, row_sum;
 
-    __forceinline__ __device__ Softmax() {};
+    __forceinline__ __device__ Softmax() {
+        #pragma unroll
+        for (int i = 0; i < size(row_max); ++i) {
+            row_max(i) = -INFINITY;
+            row_sum(i) = 0.f;
+        }
+    };
 
     template <bool Is_first, bool Check_inf = false, typename Tensor0, typename Tensor1>
     __forceinline__ __device__ void softmax(Tensor0 &acc_s, Tensor1 &acc_o) {
