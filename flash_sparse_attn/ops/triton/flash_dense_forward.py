6ce30f9398305f6323625eb7164b33e174710bde
LoserCheems <losercheems@gmail.com>
2026-04-21 17:29:23 +0800

    Refactor flash_combine references to flash_decode_combine in forward functions

--- flash_sparse_attn/ops/triton/flash_dense_forward.py
+++ flash_sparse_attn/ops/triton/flash_dense_forward.py
@@ -7,7 +7,7 @@ import triton.language as tl
 
 from flash_sparse_attn.ops.triton import (
     assert_inputs,
-    flash_combine,
+    flash_decode_combine,
     utils,
     launch_template,
     launch_grid,
@@ -743,7 +743,7 @@ def _flash_dense_attn_base_forward(
     )
 
     if is_split_kv:
-        flash_combine._flash_attn_fwd_combine(
+        flash_decode_combine._flash_attn_fwd_combine(
             out_partial,
             lse_partial,
             out,
@@ -894,7 +894,7 @@ def _flash_dense_attn_varlen_base_forward(
     )
 
     if is_split_kv:
-        flash_combine._flash_attn_fwd_combine(
+        flash_decode_combine._flash_attn_fwd_combine(
             out_partial,
             lse_partial,
             out,
