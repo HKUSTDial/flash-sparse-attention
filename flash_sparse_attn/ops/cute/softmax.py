f12fa378073412706cce72e191a59f855e8ebb8a
LoserCheems <losercheems@gmail.com>
2026-05-23 18:58:53 +0800

    Rewrite vendored CuTe namespace to flash_sparse_attn.ops.cute

--- flash_sparse_attn/ops/cute/softmax.py
+++ flash_sparse_attn/ops/cute/softmax.py
@@ -10,9 +10,9 @@ import cutlass.cute as cute
 from cutlass import Float32, Boolean
 
 from quack import layout_utils
-import flash_attn.cute.utils as utils
+import flash_sparse_attn.ops.cute.utils as utils
 from quack.cute_dsl_utils import ParamsBase
-from flash_attn.cute.seqlen_info import SeqlenInfoQK
+from flash_sparse_attn.ops.cute.seqlen_info import SeqlenInfoQK
 
 
 @dataclass
