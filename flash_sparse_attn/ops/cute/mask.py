7a7e4d5e9e51a85b84262ab805d1ed1eb37d177d
LoserCheems <losercheems@gmail.com>
2026-06-09 12:08:11 +0800

    Rewrite vendored CuTe namespace to flash_attn.cute before subtree merge

--- flash_sparse_attn/ops/cute/mask.py
+++ flash_sparse_attn/ops/cute/mask.py
@@ -9,9 +9,9 @@ import cutlass.cute as cute
 from cutlass import Float32, Int32, Uint32, const_expr, Boolean
 
 from quack import layout_utils
-import flash_sparse_attn.ops.cute.utils as utils
-from flash_sparse_attn.ops.cute.block_info import BlockInfo
-from flash_sparse_attn.ops.cute.seqlen_info import SeqlenInfoQK
+import flash_attn.cute.utils as utils
+from flash_attn.cute.block_info import BlockInfo
+from flash_attn.cute.seqlen_info import SeqlenInfoQK
 
 MaskGenFn: TypeAlias = Callable[[int], Uint32]
 MASK_R2P_CHUNK_SIZE: int = 32
