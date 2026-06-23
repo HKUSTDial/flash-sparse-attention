36bbac2509f2a35f7ad7d0fb77f41787151f8ce2
LoserCheems <losercheems@gmail.com>
2026-06-23 20:26:09 +0800

    Enhance apply_gather_mask function to incorporate actual sequence length and head dimension for improved masking logic

--- flash_sparse_attn/ops/triton/topk_gather_kv.py
+++ flash_sparse_attn/ops/triton/topk_gather_kv.py
@@ -72,6 +72,12 @@ def load_gathered_d(
 def apply_gather_mask(
     acc_s,
     topk_indices,
+    actual_seqlen_q,
+    QHEAD_PER_KVHEAD_PACKGQA: tl.constexpr,
+    TILE_M: tl.constexpr,
 ):
     valid_mask = make_valid_mask(topk_indices)
-    return tl.where(valid_mask[None, :], acc_s, float("-inf"))
+    offs_m = tl.arange(0, TILE_M)
+    q_idx = offs_m // QHEAD_PER_KVHEAD_PACKGQA
+    valid_m = q_idx < actual_seqlen_q
+    return tl.where(valid_m[:, None] & valid_mask[None, :], acc_s, float("-inf"))
