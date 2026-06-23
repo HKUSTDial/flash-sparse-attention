b566e8448c9b0516be0c3222436632774e7efc72
LoserCheems <losercheems@gmail.com>
2026-06-23 20:26:15 +0800

    Add actual sequence length and packing configuration to apply_gather_mask in AttnDecPointerScheduler

--- flash_sparse_attn/ops/triton/scheduler.py
+++ flash_sparse_attn/ops/triton/scheduler.py
@@ -2487,6 +2487,9 @@ class AttnDecPointerScheduler:
         return topk_gather_kv.apply_gather_mask(
             acc_s,
             topk_indices,
+            config.actual_seqlen_q,
+            config.QHEAD_PER_KVHEAD_PACKGQA,
+            config.TILE_M,
         )
 
 
