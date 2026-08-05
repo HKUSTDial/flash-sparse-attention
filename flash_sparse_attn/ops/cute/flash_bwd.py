005112f229e1520e37c746c4cf1bbdb2df257e0c
LoserCheems <losercheems@gmail.com>
2026-08-05 10:43:33 +0800

    feat: add optional mCuTotalMBlocks parameter to FlashAttentionBackwardSm80 and FlashSparseAttentionBackwardSm80

--- flash_sparse_attn/ops/cute/flash_bwd.py
+++ flash_sparse_attn/ops/cute/flash_bwd.py
@@ -458,6 +458,7 @@ class FlashAttentionBackwardSm80:
         mdV_semaphore: Optional[cute.Tensor] = None,
         aux_data: AuxData = AuxData(),
         blocksparse_tensors: Optional[BlockSparseTensors] = None,
+        mCuTotalMBlocks: Optional[cute.Tensor] = None,
         # Always keep stream as the last parameter (EnvStream: obtained implicitly via TVM FFI).
         stream: cuda.CUstream = None,
     ):
@@ -517,6 +518,7 @@ class FlashAttentionBackwardSm80:
             else 1,
             mCuSeqlensQ=mCuSeqlensK,
             mSeqUsedQ=mSeqUsedK,
+            cu_total_m_blocks_ptr=mCuTotalMBlocks,
         )
 
         tile_sched_params = TileScheduler.to_underlying_arguments(tile_sched_args)
@@ -2045,6 +2047,7 @@ class FlashSparseAttentionBackwardSm80:
         mdV_semaphore: Optional[cute.Tensor] = None,
         aux_data: AuxData = AuxData(),
         blocksparse_tensors: Optional[BlockSparseTensors] = None,
+        mCuTotalMBlocks: Optional[cute.Tensor] = None,
         # Always keep stream as the last parameter (EnvStream: obtained implicitly via TVM FFI).
         stream: cuda.CUstream = None,
     ):
@@ -2113,6 +2116,7 @@ class FlashSparseAttentionBackwardSm80:
             else 1,
             mCuSeqlensQ=mCuSeqlensK,
             mSeqUsedQ=mSeqUsedK,
+            cu_total_m_blocks_ptr=mCuTotalMBlocks,
         )
 
         tile_sched_params = TileScheduler.to_underlying_arguments(tile_sched_args)
