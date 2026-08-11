fb00553ea2a87f59f6da20fb0b34c9c58f74fc57
LoserCheems <losercheems@gmail.com>
2026-08-11 18:06:31 +0800

    refactor: rename window_dist to window_near in assert_fwd_inputs and assert_bwd_inputs functions

--- flash_sparse_attn/ops/triton/assert_inputs.py
+++ flash_sparse_attn/ops/triton/assert_inputs.py
@@ -115,7 +115,7 @@ def assert_fwd_inputs(
     if window_sizes is not None:
         assert window_sizes.dtype == torch.int32, "window_sizes must be int32"
         assert window_sizes.ndim == 2 and window_sizes.shape[1] == 4, (
-            "window_sizes must have shape [num_kv_heads, 4] with columns [window_sink, window_left, window_right, window_dist]"
+            "window_sizes must have shape [num_kv_heads, 4] with columns [window_sink, window_left, window_right, window_near]"
         )
 
 
@@ -239,7 +239,7 @@ def assert_bwd_inputs(
     if window_sizes is not None:
         assert window_sizes.dtype == torch.int32, "window_sizes must be int32"
         assert window_sizes.ndim == 2 and window_sizes.shape[1] == 4, (
-            "window_sizes must have shape [num_kv_heads, 4] with columns [window_sink, window_left, window_right, window_dist]"
+            "window_sizes must have shape [num_kv_heads, 4] with columns [window_sink, window_left, window_right, window_near]"
         )
 
 
@@ -350,5 +350,5 @@ def assert_dec_inputs(
     if window_sizes is not None:
         assert window_sizes.dtype == torch.int32, "window_sizes must be int32"
         assert window_sizes.ndim == 2 and window_sizes.shape[1] == 4, (
-            "window_sizes must have shape [num_kv_heads, 4] with columns [window_sink, window_left, window_right, window_dist]"
+            "window_sizes must have shape [num_kv_heads, 4] with columns [window_sink, window_left, window_right, window_near]"
         )
